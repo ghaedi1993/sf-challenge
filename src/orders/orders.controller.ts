21326567a960214fe1ae4a80713ec3b9a1ad8d65
Javad Ghaedi <ghaedi1993@gmail.com>
2023-05-17 14:53:21 +0330

    test

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -36,9 +36,7 @@ export class OrdersController {
   }
 
   @Delete(':id')
-
-
   remove(@Param('id') id: string) {
-            return this.ordersService.remove(+id);
+    return this.ordersService.remove(+id);
   }
 }
