1a5f043ab119df417276bf11f46d4b1985492e55
Javad Ghaedi <ghaedi1993@gmail.com>
2023-05-17 16:09:49 +0330

    add role

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -6,6 +6,13 @@ import {
   Table,
 } from 'sequelize-typescript';
 
+enum UserRole {
+  CUSTOMER='CUSTOMER',
+  VENDOR_STAFF='VENDOR_STAFF',
+  DELIVERY_DRIVER='DELIVERY_DRIVER',
+  AGEN='AGEN'
+}
+
 @Table
 export class User extends Model {
   @PrimaryKey
@@ -23,4 +30,10 @@ export class User extends Model {
 
   @Column
   salt: string;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(UserRole)),
+    allowNull: false,  
+  })
+  role: UserRole;
 }
