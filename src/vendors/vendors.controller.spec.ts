9ea521873a7307ab27b5572c3f531e760562f463
Javad Ghaedi <ghaedi1993@gmail.com>
2023-05-20 10:23:02 +0330

    fix vendor tests

--- src/vendors/vendors.controller.spec.ts
+++ src/vendors/vendors.controller.spec.ts
@@ -1,18 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VendorsController } from './vendors.controller';
+import { VendorsService } from './vendors.service';
+import { CreateVendorDto } from './dto/create-vendor.dto';
+import { UpdateVendorDto } from './dto/update-vendor.dto';
+import { Vendor } from './vendor.model';
+import { VendorsRepository } from './vendors.repository';
+
+// jest.mock('src/', () => ({
+//   __esModule: true,
+//   OrdersModule: {
+//     register: jest.fn(() => ({
+//       compile: jest.fn(),
+//     })),
+//   },
+// }));
 
 describe('VendorsController', () => {
-  let controller: VendorsController;
-
+  let vendorsService: VendorsService;
+  let vendorsController: VendorsController;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [VendorsController],
+      providers: [
+        VendorsController,
+        {
+          provide: VendorsService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delayReport: jest.fn()
+          },
+        }
+      ],
     }).compile();
 
-    controller = module.get<VendorsController>(VendorsController);
+    vendorsService = module.get<VendorsService>(VendorsService);
+    vendorsController = module.get<VendorsController>(VendorsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  
+
+
+  describe('create', () => {
+    it('should be defined', () => {
+      expect(vendorsService).toBeDefined();
+      expect(vendorsController).toBeDefined();
+    });
+    it('should create a new vendor', async () => {
+      const createVendorDto: CreateVendorDto = { name: 'vendor 1' };
+      const createdVendor = { id: 1, ...createVendorDto };
+
+      const createSpy = jest.spyOn(vendorsService, 'create').mockResolvedValue(createdVendor as Vendor);
+      const result = await vendorsController.create(createVendorDto);
+
+      expect(createSpy).toHaveBeenCalledWith(createVendorDto);
+      expect(result).toEqual(createdVendor);
+    });
+  });
+
+  describe('getDelayReport', () => {
+    it('should get the delay report', async () => {
+      const delayReport = { 'vendor 1': 10 };
+
+      jest.spyOn(vendorsService, 'delayReport').mockResolvedValue(delayReport);
+
+      const result = await vendorsController.getDelayReport();
+
+      expect(vendorsService.delayReport).toHaveBeenCalled();
+      expect(result).toEqual(delayReport);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should get all vendors', async () => {
+      const vendors = [{ id: 1, name: 'vendor 1' }];
+
+      jest.spyOn(vendorsService, 'findAll').mockResolvedValue(vendors as any);
+
+      const result = await vendorsController.findAll();
+
+      expect(vendorsService.findAll).toHaveBeenCalled();
+      expect(result).toEqual(vendors);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a vendor', async () => {
+      const id = '1';
+      const updateVendorDto: UpdateVendorDto = { name: 'Updated Vendor' };
+      const updatedVendor = { id: 1, name: 'Updated Vendor' };
+
+      jest.spyOn(vendorsService, 'update').mockResolvedValue(updatedVendor as any);
+
+      const result = await vendorsController.update(id, updateVendorDto);
+
+      expect(vendorsService.update).toHaveBeenCalledWith({ id: 1 }, updateVendorDto);
+      expect(result).toEqual(updatedVendor);
+    });
   });
 });
--- src/vendors/vendors.service.spec.ts
+++ src/vendors/vendors.service.spec.ts
@@ -1,18 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VendorsService } from './vendors.service';
+import { VendorsRepository } from './vendors.repository';
+import { OrdersService } from 'src/orders/orders.service';
 
 describe('VendorsService', () => {
-  let service: VendorsService;
+  let vendorsService: VendorsService;
+  let vendorsRepository: VendorsRepository;
+  let ordersService: OrdersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VendorsService],
+      providers: [
+        VendorsService,
+        {
+          provide: VendorsRepository,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+          },
+        },
+        {
+          provide: OrdersService,
+          useValue: {
+            weeklyDelayReport: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<VendorsService>(VendorsService);
+    vendorsService = module.get<VendorsService>(VendorsService);
+    vendorsRepository = module.get<VendorsRepository>(VendorsRepository);
+    ordersService = module.get<OrdersService>(OrdersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should be defined', () => {
+      expect(vendorsService).toBeDefined();
+      expect(vendorsRepository).toBeDefined();
+      expect(ordersService).toBeDefined();
+    });
+    it('should call vendorsRepository.create with the provided vendor', async () => {
+      const createSpy = jest.spyOn(vendorsRepository, 'create');
+
+      const vendor = { name: 'Vendor A' };
+      await vendorsService.create(vendor);
+
+      expect(createSpy).toHaveBeenCalledWith(vendor);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call vendorsRepository.findAll with the provided where parameter', async () => {
+      const findAllSpy = jest.spyOn(vendorsRepository, 'findAll');
+
+      const where = {};
+      await vendorsService.findAll(where);
+
+      expect(findAllSpy).toHaveBeenCalledWith(where);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call vendorsRepository.findOne', async () => {
+      const findOneSpy = jest.spyOn(vendorsRepository, 'findOne');
+
+      const where = { id: 1 };
+      await vendorsService.findOne(where);
+
+      expect(findOneSpy).toHaveBeenCalledWith(where);
+    });
+  });
+
+  describe('update', () => {
+    it('should call vendorsRepository.update with the provided where and updateVendorDto parameters', async () => {
+      const updateSpy = jest.spyOn(vendorsRepository, 'update');
+
+      const where = { id: 1 };
+      const updateVendorDto = { name: 'Updated Vendor' };
+      await vendorsService.update(where, updateVendorDto);
+
+      expect(updateSpy).toHaveBeenCalledWith(where, updateVendorDto);
+    });
+  });
+
+  describe('delayReport', () => {
+    it('should call ordersService.weeklyDelayReport', async () => {
+      const weeklyDelayReportSpy = jest.spyOn(
+        ordersService,
+        'weeklyDelayReport',
+      );
+
+      await vendorsService.delayReport();
+
+      expect(weeklyDelayReportSpy).toHaveBeenCalled();
+    });
   });
 });
